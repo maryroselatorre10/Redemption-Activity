ebc930d0d35a5e291739642f76b9be4e4672f5f4
maryroselatorre10 <60722805+maryroselatorre10@users.noreply.github.com>
2020-05-15 09:01:15 +0700

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,13 @@
+function Generate(){
+    var x = Math.floor(Math.random() *255)+1;
+    var y = Math.floor(Math.random() *255)+1;
+    var z = Math.floor(Math.random() *255)+1;
+    document.querySelector("img").style.backgroundColor = "rgb(" +x+ "," +y+ "," +z+ ")" ;
+}
+function Show(){
+    var colors = document.querySelector("select").value;
+    document.querySelector("img").style.backgroundColor = colors;
+}
+function Reset(){
+    document.querySelector("img").style.backgroundColor = "white";
+}
